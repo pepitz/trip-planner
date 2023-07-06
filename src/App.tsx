60d6a5c9acad62af2a70f05336c6b21e80b6f842
Petar Tsenev <petar.tzenev@gmail.com>
2023-07-06 17:12:25 +0200

    feat: add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-function App() {
-  return <div>App</div>;
-}
+import Home from 'pages/Home/Home';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>;
+};
 
 export default App;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,4 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
 
 declare module '@mui/material/styles/createPalette' {
   interface PaletteOptions {
