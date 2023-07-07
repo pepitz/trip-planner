3dae00273d4df544e3138080d546d3bec8517c65
Petar Tsenev <petar.tzenev@gmail.com>
2023-07-07 16:05:34 +0200

    feat: add CustomIcon component

---
+++ src/components/CustomIcon/CustomIcon.tsx
@@ -0,0 +1,24 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+const CustomIcon = ({
+  icon,
+  width,
+  height,
+  viewBox,
+  fill,
+  ...otherProps
+}: any): JSX.Element => {
+  return (
+    <SvgIcon {...otherProps}>
+      <svg
+        width={width || 14}
+        height={height || 14}
+        viewBox={viewBox || '0 0 14 14'}
+      >
+        <g fill={fill ?? 'currentColor'}>{icon}</g>
+      </svg>
+    </SvgIcon>
+  );
+};
+
+export default CustomIcon;
