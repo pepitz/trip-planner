1f3f2245410a6256dae37ed6bcc6987c90cd0fee
Petar Tsenev <petar.tzenev@gmail.com>
2023-07-11 00:09:21 +0200

    feat(component): addDestination comp

---
+++ src/components/AddDestinationButton/AddDestinationButton.tsx
@@ -0,0 +1,31 @@
+import { Box, IconButton } from '@mui/material';
+
+import CustomIcon from 'components/CustomIcon/CustomIcon';
+import icons from 'enums/icons';
+
+import './AddDestinationButton.scss';
+
+interface IAddDestinationButton {
+  handleAddCity: () => void;
+}
+
+const AddDestinationButton = ({
+  handleAddCity,
+}: IAddDestinationButton): JSX.Element => {
+  return (
+    <Box
+      sx={{ color: 'others.purpleDark' }}
+      component="section"
+      className="path__container--add-city"
+    >
+      <label htmlFor="addButton">
+        <IconButton id="addButton" onClick={handleAddCity}>
+          <CustomIcon fill="transparent" icon={icons.plus_outlined} />
+        </IconButton>
+        Add destination
+      </label>
+    </Box>
+  );
+};
+
+export default AddDestinationButton;
