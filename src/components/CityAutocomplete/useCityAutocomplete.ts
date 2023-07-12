df3da9ee0e48060210719419b443925027e861b8
Petar Tsenev <petar.tzenev@gmail.com>
2023-07-12 16:50:24 +0200

    fix: move logic to useCityAutocomplete

---
+++ src/components/CityAutocomplete/useCityAutocomplete.ts
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react';
+
+import { ICityAutocompleteProps } from 'types';
+import * as CONSTANTS from 'constants/index';
+
+const useCityAutocomplete = () => {
+  const [cities, setCities] = useState<ICityAutocompleteProps[]>([]);
+
+  const handleAddCity = () => {
+    setCities((prevValue) => {
+      let updatedCities = [...prevValue];
+      const lastIndex = updatedCities.length - 1;
+
+      updatedCities[lastIndex].name = `intermediate_${lastIndex}`;
+      updatedCities[lastIndex].canBeDeleted = true;
+
+      const newRecord = { ...updatedCities[lastIndex] };
+      newRecord.index += 1;
+      newRecord.name = CONSTANTS.DESTINATION;
+      newRecord.value = '';
+
+      updatedCities.push(newRecord);
+      return updatedCities;
+    });
+  };
+  const handleRemoveCity = (index: number) => {
+    setCities((prevFields) => {
+      let updated = [...prevFields];
+      updated = updated.filter((city) => city.index !== index);
+      updated.forEach((city, idx, arr) => {
+        if (city.name !== CONSTANTS.ORIGIN) {
+          if (city.index === updated.length - 1) {
+            city.name = CONSTANTS.DESTINATION;
+            city.canBeDeleted = true;
+          }
+          if (idx === 1 && arr.length === 2) {
+            city.name = CONSTANTS.DESTINATION;
+            city.canBeDeleted = false;
+          }
+          city.index = idx;
+        }
+      });
+      return updated;
+    });
+  };
+
+  useEffect(() => {
+    const initialCities = [
+      {
+        index: 0,
+        name: CONSTANTS.ORIGIN,
+        label: CONSTANTS.CITY_ORIGIN,
+        value: '',
+        setValue: setCities,
+        canBeDeleted: false,
+        handleDelete: handleRemoveCity,
+      },
+      {
+        index: 1,
+        name: CONSTANTS.DESTINATION,
+        label: CONSTANTS.CITY_DESTINATION,
+        value: '',
+        setValue: setCities,
+        canBeDeleted: false,
+        handleDelete: handleRemoveCity,
+      },
+    ];
+    setCities(initialCities);
+  }, []);
+
+  useEffect(() => {
+    console.log('cities: ', cities);
+  }, [cities]);
+
+  return {
+    cities,
+    handleAddCity,
+  };
+};
+
+export default useCityAutocomplete;
--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,93 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Box from '@mui/material/Box';
 
-import * as CONSTANTS from 'constants/index';
-import { ICityAutocompleteProps } from 'types';
-
 import TimeLine from 'components/TravelLine/TravelLine';
-import CityAutocomplete from 'components/common/CityAutocomplete';
+import CityAutocomplete from 'components/CityAutocomplete/CityAutocomplete';
+import useCityAutocomplete from 'components/CityAutocomplete/useCityAutocomplete';
+
 import AddDestinationButton from 'components/AddDestinationButton/AddDestinationButton';
 import PassengersCounter from 'components/PassengersCounter/PassengersCounter';
 
-import './SearchForm.scss';
 import DateSelector from 'components/DateSelector/DateSelector';
 import useDateSelector from 'components/DateSelector/useDateSelector';
+import './SearchForm.scss';
 
 const SearchForm = () => {
-  const [cities, setCities] = useState<ICityAutocompleteProps[]>([]);
+  const { cities, handleAddCity } = useCityAutocomplete();
   const [numberPassengers, setNumberPassengers] = useState(0);
   const [isPassengersValid, setPassengersValid] = useState(true);
   const { date, setDate, isDateValid, setDateValid } = useDateSelector();
 
-  const handleAddCity = () => {
-    setCities((prevValue) => {
-      let updatedCities = [...prevValue];
-      const lastIndex = updatedCities.length - 1;
-
-      updatedCities[lastIndex].name = `intermediate_${lastIndex}`;
-      updatedCities[lastIndex].canBeDeleted = true;
-
-      const newRecord = { ...updatedCities[lastIndex] };
-      newRecord.index += 1;
-      newRecord.name = CONSTANTS.DESTINATION;
-      newRecord.value = '';
-
-      updatedCities.push(newRecord);
-      return updatedCities;
-    });
-  };
-
-  const handleRemoveCity = (index: number) => {
-    setCities((prevFields) => {
-      let updated = [...prevFields];
-      updated = updated.filter((city) => city.index !== index);
-      updated.forEach((city, idx, arr) => {
-        if (city.name !== CONSTANTS.ORIGIN) {
-          if (city.index === updated.length - 1) {
-            city.name = CONSTANTS.DESTINATION;
-            city.canBeDeleted = true;
-          }
-          if (idx === 1 && arr.length === 2) {
-            city.name = CONSTANTS.DESTINATION;
-            city.canBeDeleted = false;
-          }
-          city.index = idx;
-        }
-      });
-      return updated;
-    });
-  };
-
   const handleSubmit = () => console.log('paint data');
-
-  useEffect(() => {
-    const initialCities = [
-      {
-        index: 0,
-        name: CONSTANTS.ORIGIN,
-        label: CONSTANTS.CITY_ORIGIN,
-        value: '',
-        setValue: setCities,
-        canBeDeleted: false,
-        handleDelete: handleRemoveCity,
-      },
-      {
-        index: 1,
-        name: CONSTANTS.DESTINATION,
-        label: CONSTANTS.CITY_DESTINATION,
-        value: '',
-        setValue: setCities,
-        canBeDeleted: false,
-        handleDelete: handleRemoveCity,
-      },
-    ];
-    setCities(initialCities);
-  }, []);
-
-  useEffect(() => {
-    console.log('cities: ', cities);
-  }, [cities]);
 
   return (
     <Box
